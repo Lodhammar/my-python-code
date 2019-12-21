f74bfd37fb259ab63439849d6b0c166c8851083d
OneWhisper248 <58824930+OneWhisper248@users.noreply.github.com>
2019-12-21 23:18:50 +0000

    Create printing.py

---
+++ printing.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+
+#The print would put any text that you write within the parentheses.
+#The speech marks are for something called a string.
+#You can type any text in a string.
+
+print("I like coding")
+
+#If you ran this in an IDE, this would show up in the output box...
+I like coding
